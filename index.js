c415e3bc16ac501c8f4ba222f85df75eb321a098
anamikagain559 <anamika.zaimahtech@gmail.com>
2024-08-17 14:14:30 +0600

    product added

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+  
 
     const productCollection = client.db('ecommerce').collection('products');
 
@@ -58,7 +57,7 @@ async function run() {
       const count = await productCollection.estimatedDocumentCount();
       res.send({ count });
     })
-    // Send a ping to confirm a successful connection
+  
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
